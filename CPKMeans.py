184127e42d0ed5c84cdf292d6977b936a31004ac
takuyara <takuyara2136@gmail.com>
2021-08-21 12:23:28 +0800

    Update CPKMeans.py

--- CPKMeans.py
+++ CPKMeans.py
@@ -42,27 +42,38 @@ class CPKMeans:
 		# Initilization
 		self.points = points
 		self.num_clust = num_clust
+		self.penalty = W
 		self.dset = DisjointSet(len(num_points))
 		for i, j in must_link:
 			self.dset.merge(i, j)
-		self.cannot_link = {}
-		for i, j in cannot_link:
-			i = self.dset.get(i)
-			j = self.dset.get(j)
-			self.cannot_link[ts(i, j)] = True
+		self.clust_idx = []
+		for i in range(self.points.shape[0]):
+			self.clust_idx.append(None)
 
 		# Init sets
 		sizes = self.dset.get_sizes()
+		self.cannot_link = [{} for __ in range(len(sizes))]
+		for i, j in cannot_link:
+			i = self.dset.get(i)
+			j = self.dset.get(j)
+			self.cannot_link[i][j] = True
+			self.cannot_link[j][i] = True
+
+		self.neighbour_size = [0 for __ in range(len(sizes))]
+		for idx, curclust in sizes:
+			self.neighbour_size[idx] = len(curclust)
+
 		num_sets = len(sizes)
 		for i, (__, curclust) in enumerate(sizes):
 			if len(curclust) == 1:
 				num_sets = i
 				break
+
 		self.clust = []
 		self.centre = []
 		if num_sets >= num_clust:
-			for __, curclust in sizes[ : num_clust]:
-				self.insert_clust(curclust)
+			for idx, curclust in sizes[ : num_clust]:
+				self.insert_clust(idx, curclust)
 			for __, curclust in sizes[num_clust : ]:
 				for i in curclust:
 					self.assign_clust(i)
@@ -70,7 +81,7 @@ class CPKMeans:
 			clust_ids = []
 			waiting_list = []
 			for idx, curclust in sizes[ : num_sets]:
-				self.insert_clust(curclust)
+				self.insert_clust(idx, curclust)
 				clust_ids.append(idx)
 			rest_cnt = num_clust - num_sets
 			for idx, curclust in sizes[num_sets : ]:
@@ -80,7 +91,7 @@ class CPKMeans:
 						all_cannot = False
 						break
 				if all_cannot and rest_cnt > 0:
-					self.insert_clust(curclust)
+					self.insert_clust(idx, curclust)
 					rest_cnt -= 1
 				else:
 					waiting_list.extend(curclust)
@@ -88,38 +99,56 @@ class CPKMeans:
 			for __ in range(rest_cnt):
 				perturbation = np.random.randn(self.points[0].shape)
 				curpt = global_mean + perturbation
-				self.clust.append([])
+				self.clust.append({})
 				self.centre.append(curpt)
 			for i in waiting_list:
 				self.assign_clust(i)
-
-	def should_link(self, i, j):
-		return 1. if self.dset.get(i) == self.dset.get(j) else 0.
-
-	def shouldnot_link(self, i, j):
-		return 1. if ts(self.dset.get(i), self.dset.get(j)) in self.cannot_link else 0.
 
 	def compute_centre(self, clust_ids):
 		clust = self.points[clust_ids]
 		return np.mean(clust, axis = 0)
 
-	def insert_clust(self, clust_ids):
-		self.clust.append(clust_ids)
+	def insert_clust(self, idx, clust_ids):
+		self.clust.append({idx : len(clust_ids)})
 		self.centre.append(self.compute_centre(clust_ids))
 
-	def compute_nearest_clust(self, idx):
-		pass
+	def compute_nearest_clust(self, x):
+		xrt = self.dset.get(x)
+		must_link_size = self.neighbour_size[xrt]
+		min_dist = 1e100
+		for i, clust in enumerate(self.clust):
+			curd = np.linalg.norm(self.points[x] - self.centre[i])
+			curd += self.penalty * (must_link_size - clust.get(xrt, default = 0))
+			for idx, size in clust.items():
+				if idx in self.cannot_link[xrt]:
+					curd += self.penalty * size
+			if curd < min_dist:
+				min_dist = curd
+				res_clust = i
+		return res_clust
 
 	def assign_clust(self, i):
 		j = self.compute_nearest_clust(i)
-		self.clust[j].append(i)
+		rt = self.dset.get(i)
+		if rt not in self.clust[j]:
+			self.clust[j][rt] = 0
+		self.clust[j][rt] += 1
 
 	def __call__(iteration_times = 100):
 		for __ in range(iteration_times):
-			for i in range(len(self.points)):
-				self.assign_clust(i)
-			for i, clust in enumerate(self.clust):
-				self.centre[i] = compute_centre(clust)
-
-def ts(x, y):
-	return (x, y) if x < y else (y, x)
+			for i in range(self.points.shape[0]):
+				self.clust_idx[i] = self.compute_nearest_clust(i)
+			for i in range(self.num_clust):
+				self.clust[i] = {}
+			tmp_clust = {}
+			for i in range(self.points.shape[0]):
+				j = self.dset.get(i)
+				rt = self.clust_idx[i]
+				if j not in self.clust[rt]:
+					self.clust[rt] = 0
+				self.clust[rt] += 1
+				if rt not in tmp_clust:
+					tmp_clust[rt] = []
+				tmp_clust[rt].append(i)
+			for i, clust_ids in enumerate(tmp_clust):
+				self.centre[i] = compute_centre(clust_ids)
